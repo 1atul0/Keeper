5a5b0c6340aaf718c8dfb3ec23a41663942ed9fb
1atul0 <919atul@gmail.com>
2023-05-27 15:16:58 +0530

    dummy data putted

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import Note from './Note';
+import Footer from './Footer';
+import notes from "../notes"
+
+//this is 
+// function createNotes(note){
+//   return <Note key={note.key} title={note.title} content={note.content}/>
+// }
+
+function App(){
+  return <div>
+    <Header/>
+    {notes.map(note=><Note key={note.key} title={note.title} content={note.content}/>)}
+    <Footer/>
+    </div>;
+}
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function Footer(){
+  const d=new Date().getFullYear();
+  return <footer><p>copyright @ {d}</p></footer>
+}
+export default Footer;
