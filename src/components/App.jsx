f2ef41890245a098993649d9b20c9f8aeb9a26be
1atul0 <919atul@gmail.com>
2023-05-31 22:57:52 +0530

    add and delete button added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Note from './Note';
 import Footer from './Footer';
-import notes from "../notes"
+// import notes from "../notes"
 import CreateArea from "./CreateArea"
 //this is 
 // function createNotes(note){
@@ -10,10 +10,26 @@ import CreateArea from "./CreateArea"
 // }
 
 function App(){
+  const [notes,setNotes]=useState([]);
+  function addNote(newNote){
+    // console.log(note);
+    setNotes((prevNotes)=>{
+      return [...prevNotes,newNote];
+    });
+  }
+  function deleteNote(id){
+    console.log("call for delete");
+    setNotes((prevNotes)=>{
+      return prevNotes.filter((note,index)=>{
+        
+        return id!==index;
+      })
+    })
+  }
   return <div>
     <Header/>
-    <CreateArea/>
-    {notes.map(note=><Note key={note.key} title={note.title} content={note.content}/>)}
+    <CreateArea onAdd={addNote}/>
+    {notes.map((note,index)=><Note onDelete={deleteNote}key={index} id={index}title={note.title} content={note.content}/> )}
     <Footer/>
     </div>;
 }
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-function CreateArea(){
+import React ,{useState}from 'react'
+function CreateArea(props){
+  const [note,setNote]=useState({
+    title:"",
+    content:""
+  });
+  function handleChange(event){
+    const {name,value}=event.target;
+    //holod the prevValue of tilte or note
+    setNote((prevValue)=>{
+      return{
+        ...prevValue,
+        [name]:value
+      }
+    })
+  }
+  function submitNote(event){
+    props.onAdd(note);
+    setNote({title:"",content:""});//title and content get disappear after form submission
+    event.preventDefault();//stop notes get refreshed after form submit
+  }
+
   return <div>
    <form>
-    <input type='text' placeholder='Title'></input>
-    <textarea placeholder='Take a note...'></textarea>
-    <button>Add</button>
+    <input onChange={handleChange} name="title" value={note.title}type='text' placeholder='Title'></input>
+    <textarea onChange={handleChange} name="content"  value={note.content}placeholder='Take a note...'></textarea>
+    <button onClick={submitNote}>Add</button>
    </form>
   </div>
 }
